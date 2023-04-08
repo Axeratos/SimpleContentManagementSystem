e4b9046176467c689539d8d72ac9ef8d2941791b
axeratos <nikitalukianitcares@gmail.com>
2023-04-08 13:48:07 +0300

    Start scripts created

---
+++ docker/app/start.sh
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env bash
+
+# Exit whenever it encounters an error, also known as a non–zero exit code.
+set -o errexit
+# Return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status,
+#   or zero if all commands in the pipeline exit successfully.
+set -o pipefail
+# Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error when performing parameter expansion.
+set -o nounset
+# Print a trace of commands.
+set -o xtrace
+
+# Apply database migrations.
+cd app
+alembic upgrade head
+cd ..
+
+# Run application.
+python app/main.py
