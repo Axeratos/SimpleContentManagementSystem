4c57e21bd0a891e185781315bf44b6ffd286e61f
axeratos <nikitalukianitcares@gmail.com>
2023-04-08 12:01:54 +0300

    Validate post title

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -1,12 +1,18 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class BasePostSchema(BaseModel):
     title: str | None
     content: str | None
     created_at: datetime | None
+
+    @validator("title")
+    def validate_title(cls, value):
+        if value and len(value) > 100:
+            raise ValueError("Title is too long")
+        return value
 
 
 class PostCreate(BasePostSchema):
