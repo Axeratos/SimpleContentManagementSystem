2fcc31679450f1f558052643690394ab39ff27da
axeratos <nikitalukianitcares@gmail.com>
2023-04-08 10:48:59 +0300

    Initializing flask app in function

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
-from flask import Flask, request, session
+from flask import Flask
 
 from app.api.api_v1.api_register import api_router
 from app.core.config import app_config
 
-app = Flask(__name__)
-app.secret_key = app_config.APP_SECRET_KEY
 
-app.register_blueprint(api_router)
-
-
-@app.get("/")
-def main_route():
-    return {"msg": "Hello, I am working"}
+def create_app() -> Flask:
+    flask_app = Flask(__name__)
+    flask_app.secret_key = app_config.APP_SECRET_KEY
+    flask_app.register_blueprint(api_router)
+    return flask_app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
